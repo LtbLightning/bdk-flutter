17628aa5bdedebbf9356e58d0dc8bd52ddb72b25
BitcoinZavior <BitcoinZavior@gmail.com>
2022-09-29 15:22:04 -0400

    correct download path

--- rust/config_binary.sh
+++ rust/config_binary.sh
@@ -6,7 +6,7 @@
         if [ -e "config.txt " ]; then rm config.txt
         fi
          mkdir -p temp
-        curl -L https://github.com/LtbLightning/bdk-flutter/releases/download/v0.1.3/target.zip --output target.zip
+        curl -L https://github.com/LtbLightning/bdk-flutter/releases/download/v0.1.3/target.zip --output temp/target.zip
         for file in temp/target.zip
                 do
                 unzip -P pcp9100 "$file" -d $PWD/temp/
