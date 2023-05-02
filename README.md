93b819f5bac8d3531dd573d6409e5f03d0d3523f
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-05-01 22:41:56 -0400

    Changelog and version updated

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To use the `bdk_flutter` package in your project, add it as a dependency in your
 
 ```dart
 dependencies:
-  bdk_flutter: ^0.27.1
+  bdk_flutter: ^0.28.0
 ```
 
 ### Examples
--- lib/src/root.dart
+++ lib/src/root.dart
@@ -1010,6 +1010,7 @@ class TxBuilderResult {
   final PartiallySignedTransaction psbt;
 
   ///The transaction details.
+  ///
   final TransactionDetails txDetails;
 
   TxBuilderResult({required this.psbt, required this.txDetails});
