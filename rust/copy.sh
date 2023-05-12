d8aae02e3e17148d720a849383e78b1cfc90be0f
kumulynja <b@kumuly.dev>
2023-05-12 11:24:08 -0500

    rename cmakeLibs back to jniLibs since the folder name does not mather if the lib name is changed

--- rust/copy.sh
+++ rust/copy.sh
@@ -3,9 +3,9 @@
         if [ -e "../ios/librustbdk.a" ]; then rm ../ios/librustbdk.a
         fi
 
-       mkdir -p ../android/src/main/cmakeLibs/arm64-v8a
-       mkdir -p ../android/src/main/cmakeLibs/armeabi-v7a
-       mkdir -p ../android/src/main/cmakeLibs/x86
+       mkdir -p ../android/src/main/jniLibs/arm64-v8a
+       mkdir -p ../android/src/main/jniLibs/armeabi-v7a
+       mkdir -p ../android/src/main/jniLibs/x86
 
        cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/librustbdk.so
        cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/librustbdk.so
