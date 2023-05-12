ef4d1dec59941ed8597035ce2c54043793821024
kumulynja <b@kumuly.dev>
2023-05-12 10:36:46 -0500

    undo folder renaming and instead rename librust.so to librustsdk.so too

--- rust/copy.sh
+++ rust/copy.sh
@@ -1,15 +1,15 @@
        if [ -d "../android/src/main/jniLibs" ]; then rm -r ../android/src/main/jniLibs
         fi
-        if [ -e "../ios/librust.a" ]; then rm ../ios/librust.a
+        if [ -e "../ios/librustbdk.a" ]; then rm ../ios/librustbdk.a
         fi
 
        mkdir -p ../android/src/main/cmakeLibs/arm64-v8a
        mkdir -p ../android/src/main/cmakeLibs/armeabi-v7a
        mkdir -p ../android/src/main/cmakeLibs/x86
 
-       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/cmakeLibs/arm64-v8a
-       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/cmakeLibs/armeabi-v7a
-       cp target/i686-linux-android/release/librust.so  ../android/src/main/cmakeLibs/x86
+       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/librustbdk.so
+       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/librustbdk.so
+       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/librustbdk.so
 
        cp target/universal/release/librust.a  ../ios/librustbdk.a
 
