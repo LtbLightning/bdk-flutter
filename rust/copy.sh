8ea7c3a88367d50cd39ad15ef6f2b89be7389d82
kumulynja <b@kumuly.dev>
2023-05-12 12:45:24 -0500

    add comment to explain the renaming of the libs + new build

--- rust/copy.sh
+++ rust/copy.sh
@@ -1,15 +1,16 @@
        if [ -d "../android/src/main/jniLibs" ]; then rm -r ../android/src/main/jniLibs
         fi
-        if [ -e "../ios/librustbdk.a" ]; then rm ../ios/librustbdk.a
+        if [ -e "../ios/libbdk.a" ]; then rm ../ios/libbdk.a
         fi
 
        mkdir -p ../android/src/main/jniLibs/arm64-v8a
        mkdir -p ../android/src/main/jniLibs/armeabi-v7a
        mkdir -p ../android/src/main/jniLibs/x86
 
-       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/librustbdk.so
-       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/librustbdk.so
-       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/librustbdk.so
+       # copy and rename from librust to libbdk to avoid conflicts with other librust packages
+       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/libbdk.so
+       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/libbdk.so
+       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/libbdk.so
 
-       cp target/universal/release/librust.a  ../ios/librustbdk.a
+       cp target/universal/release/librust.a  ../ios/libbdk.a
 
