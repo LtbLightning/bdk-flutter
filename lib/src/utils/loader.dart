c614de4c436dacb75e72dc5da456e40d7281d245
kumulynja <b@kumuly.dev>
2023-05-14 15:35:55 -0500

    change binary names to librust_bdk_ffi

--- lib/src/utils/loader.dart
+++ lib/src/utils/loader.dart
@@ -5,5 +5,5 @@ import '../generated/bindings.dart';
 
 final dylib = Platform.isIOS
     ? DynamicLibrary.process()
-    : DynamicLibrary.open('libbdk.so');
+    : DynamicLibrary.open('librust_bdk_ffi.so');
 final loaderApi = RustImpl(dylib);
--- rust/copy.sh
+++ rust/copy.sh
@@ -1,15 +1,15 @@
        if [ -d "../android/src/main/jniLibs" ]; then rm -r ../android/src/main/jniLibs
         fi
-        if [ -e "../ios/libbdk.a" ]; then rm ../ios/libbdk.a
+        if [ -e "../ios/librust_bdk_ffi.a" ]; then rm ../ios/librust_bdk_ffi.a
         fi
 
        mkdir -p ../android/src/main/jniLibs/arm64-v8a
        mkdir -p ../android/src/main/jniLibs/armeabi-v7a
        mkdir -p ../android/src/main/jniLibs/x86
-       # Name the native lib 'libbdk' when copying to avoid naming conflicts with other libs generated from a rust folder with flutter_rust_bridge
-       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/libbdk.so
-       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/libbdk.so
-       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/libbdk.so
+       # Name the native lib 'librust_bdk_ffi' when copying to avoid naming conflicts with other libs generated from a rust folder with flutter_rust_bridge
+       cp target/aarch64-linux-android/release/librust.so  ../android/src/main/jniLibs/arm64-v8a/librust_bdk_ffi.so
+       cp target/armv7-linux-androideabi/release/librust.so  ../android/src/main/jniLibs/armeabi-v7a/librust_bdk_ffi.so
+       cp target/i686-linux-android/release/librust.so  ../android/src/main/jniLibs/x86/librust_bdk_ffi.so
 
-       cp target/universal/release/librust.a  ../ios/libbdk.a
+       cp target/universal/release/librust.a  ../ios/librust_bdk_ffi.a
 
