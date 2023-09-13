fb99761e4c98d506bd99571e2b2d8d36ecbaa4b8
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-09-13 07:19:26 -0400

    Code cleanup for macOS support

--- example/android/app/src/main/kotlin/io/bdk/f/flutter_bdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.bdk.f.flutter_bdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/flutter_bdk.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_bdk_platform_interface.dart';
-
-class FlutterBdk {
-  Future<String?> getPlatformVersion() {
-    return FlutterBdkPlatform.instance.getPlatformVersion();
-  }
-}
