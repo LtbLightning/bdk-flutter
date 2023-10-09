931a9fe388894e334c233f398d6caa6ceaffa4a3
BitcoinZavior <BitcoinZavior@GMail.Com>
2023-10-08 22:15:16 -0400

    rust build.rs updated; Macos bug resolved

--- rust/build.rs
+++ rust/build.rs
@@ -9,7 +9,9 @@ const RUST_INPUT: &str = "src/r_api.rs";
 /// Path of output generated Dart code
 const DART_OUTPUT: &str = "../lib/src/generated/bindings.dart";
 const C_OUTPUT: &str = "../ios/Classes/bindings.h";
+const MACOS_C_OUTPUT: &str = "../macos/Classes/";
 const DECL_OUTPUT: &str = "../lib/src/generated/bridge_definitions.dart";
+
 fn main() {
     // Tell Cargo that if the input Rust code changes, to rerun this build script.
     println!("cargo:rerun-if-changed={}", RUST_INPUT.to_string());
@@ -24,7 +26,9 @@ fn main() {
         // for other options use defaults
         wasm: false,
         dart_decl_output: Some(DECL_OUTPUT.into()),
+
         c_output: Some(vec![C_OUTPUT.to_string()]),
+        extra_c_output_path: Some(vec![MACOS_C_OUTPUT.to_string()]),
         inline_rust: true,
         dart_format_line_length: 120,
 
