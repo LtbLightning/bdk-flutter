7ec34440b768b0c11e914f700e50137b5e77677f
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-04-18 21:57:00 -0400

    updated usePrecompiledBinaries default to true

--- cargokit/build_tool/lib/src/options.dart
+++ cargokit/build_tool/lib/src/options.dart
@@ -298,7 +298,10 @@ class CargokitUserOptions {
       }
       userProjectDir = userProjectDir.parent;
     }
-    return CargokitUserOptions._();
+    return CargokitUserOptions(
+      usePrecompiledBinaries: true,
+      verboseLogging: false,
+    );
   }
 
   final bool usePrecompiledBinaries;
