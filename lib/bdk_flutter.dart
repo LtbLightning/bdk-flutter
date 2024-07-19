bff118547bc3796bf33a21a3c5730f63903b5129
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-07-19 13:51:00 -0400

    resolved flutter analyze error

--- lib/bdk_flutter.dart
+++ lib/bdk_flutter.dart
@@ -6,31 +6,20 @@ export './src/generated/api/blockchain.dart'
         BdkBlockchain,
         BlockchainConfig_Electrum,
         BlockchainConfig_Esplora,
-        AnyBlockchain,
         Auth_Cookie,
         Auth_UserPass,
         Auth_None,
         BlockchainConfig_Rpc;
-export './src/generated/api/descriptor.dart'
-    hide
-        BdkDescriptor,
-        ExtendedDescriptor,
-        KeysDescriptorSecretKey,
-        KeysDescriptorPublicKey,
-        KeysKeyMap;
+export './src/generated/api/descriptor.dart' hide BdkDescriptor;
 export './src/generated/api/key.dart'
     hide
         BdkDerivationPath,
         BdkDescriptorPublicKey,
         BdkDescriptorSecretKey,
-        BitcoinBip32DerivationPath,
-        KeysBip39Mnemonic,
         BdkMnemonic;
-export './src/generated/api/psbt.dart'
-    hide MutexPartiallySignedTransaction, BdkPsbt;
+export './src/generated/api/psbt.dart' hide BdkPsbt;
 export './src/generated/api/types.dart'
     hide
-        BdkBitcoinAddress,
         BdkScriptBuf,
         BdkTransaction,
         AddressIndex_Reset,
@@ -49,10 +38,6 @@ export './src/generated/api/types.dart'
         RbfValue_Value,
         DatabaseConfig_Sqlite;
 export './src/generated/api/wallet.dart'
-    hide
-        MutexBdkWalletAnyDatabase,
-        BdkWallet,
-        finishBumpFeeTxBuilder,
-        txBuilderFinish;
+    hide BdkWallet, finishBumpFeeTxBuilder, txBuilderFinish;
 export './src/root.dart';
 export 'src/utils/exceptions.dart' hide mapBdkError, BdkFfiException;
