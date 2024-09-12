7176ecbeaab014def8acf5dddbe5bec9002ccffc
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-09-12 08:27:00 -0400

    handle_mutex exposed to handle lock errors

--- rust/src/api/mod.rs
+++ rust/src/api/mod.rs
@@ -1,3 +1,7 @@
+use std::{fmt::Debug, sync::Mutex};
+
+use error::BdkError;
+
 pub mod blockchain;
 pub mod descriptor;
 pub mod error;
@@ -5,3 +9,17 @@ pub mod key;
 pub mod psbt;
 pub mod types;
 pub mod wallet;
+
+pub(crate) fn handle_mutex<T, F, R>(lock: &Mutex<T>, operation: F) -> Result<R, BdkError>
+where
+    T: Debug,
+    F: FnOnce(&mut T) -> R,
+{
+    match lock.lock() {
+        Ok(mut mutex_guard) => Ok(operation(&mut *mutex_guard)),
+        Err(poisoned) => {
+            drop(poisoned.into_inner());
+            Err(BdkError::Generic("Poison Error!".to_string()))
+        }
+    }
+}
