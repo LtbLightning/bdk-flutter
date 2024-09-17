2aa04feb5d6e2b430a04a4b61007ae0a352b47d7
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-09-17 01:36:00 -0400

    implemeted handle_mutex

--- rust/src/api/psbt.rs
+++ rust/src/api/psbt.rs
@@ -8,6 +8,8 @@ use std::str::FromStr;
 
 use flutter_rust_bridge::frb;
 
+use super::handle_mutex;
+
 #[derive(Debug)]
 pub struct BdkPsbt {
     pub ptr: RustOpaque<std::sync::Mutex<bdk::bitcoin::psbt::PartiallySignedTransaction>>,
@@ -28,43 +30,51 @@ impl BdkPsbt {
     }
 
     #[frb(sync)]
-    pub fn as_string(&self) -> String {
-        let psbt = self.ptr.lock().unwrap().clone();
-        psbt.to_string()
+    pub fn as_string(&self) -> Result<String, BdkError> {
+        handle_mutex(&self.ptr, |psbt| psbt.to_string())
     }
 
     ///Computes the `Txid`.
     /// Hashes the transaction excluding the segwit data (i. e. the marker, flag bytes, and the witness fields themselves).
     /// For non-segwit transactions which do not have any segwit data, this will be equal to transaction.wtxid().
     #[frb(sync)]
-    pub fn txid(&self) -> String {
-        let tx = self.ptr.lock().unwrap().clone().extract_tx();
-        let txid = tx.txid();
-        txid.to_string()
+    pub fn txid(&self) -> Result<String, BdkError> {
+        handle_mutex(&self.ptr, |psbt| {
+            psbt.to_owned().extract_tx().txid().to_string()
+        })
     }
 
     /// Return the transaction.
     #[frb(sync)]
     pub fn extract_tx(ptr: BdkPsbt) -> Result<BdkTransaction, BdkError> {
-        let tx = ptr.ptr.lock().unwrap().clone().extract_tx();
-        tx.try_into()
+        handle_mutex(&ptr.ptr, |psbt| {
+            let tx = psbt.to_owned().extract_tx();
+            tx.try_into()
+        })?
     }
 
     /// Combines this PartiallySignedTransaction with other PSBT as described by BIP 174.
     ///
     /// In accordance with BIP 174 this function is commutative i.e., `A.combine(B) == B.combine(A)`
     pub fn combine(ptr: BdkPsbt, other: BdkPsbt) -> Result<BdkPsbt, BdkError> {
-        let other_psbt = other.ptr.lock().unwrap().clone();
-        let mut original_psbt = ptr.ptr.lock().unwrap().clone();
+        let other_psbt = other
+            .ptr
+            .lock()
+            .map_err(|_| BdkError::Generic("Poison Error!".to_string()))?
+            .clone();
+        let mut original_psbt = ptr
+            .ptr
+            .lock()
+            .map_err(|_| BdkError::Generic("Poison Error!".to_string()))?;
         original_psbt.combine(other_psbt)?;
-        Ok(original_psbt.into())
+        Ok(original_psbt.to_owned().into())
     }
 
     /// The total transaction fee amount, sum of input amounts minus sum of output amounts, in Sats.
     /// If the PSBT is missing a TxOut for an input returns None.
     #[frb(sync)]
-    pub fn fee_amount(&self) -> Option<u64> {
-        self.ptr.lock().unwrap().fee_amount()
+    pub fn fee_amount(&self) -> Result<Option<u64>, BdkError> {
+        handle_mutex(&self.ptr, |psbt| psbt.fee_amount())
     }
 
     /// The transaction's fee rate. This value will only be accurate if calculated AFTER the
@@ -72,20 +82,20 @@ impl BdkPsbt {
     /// transaction.
     /// If the PSBT is missing a TxOut for an input returns None.
     #[frb(sync)]
-    pub fn fee_rate(&self) -> Option<FeeRate> {
-        self.ptr.lock().unwrap().fee_rate().map(|e| e.into())
+    pub fn fee_rate(&self) -> Result<Option<FeeRate>, BdkError> {
+        handle_mutex(&self.ptr, |psbt| psbt.fee_rate().map(|e| e.into()))
     }
 
     ///Serialize as raw binary data
     #[frb(sync)]
-    pub fn serialize(&self) -> Vec<u8> {
-        let psbt = self.ptr.lock().unwrap().clone();
-        psbt.serialize()
+    pub fn serialize(&self) -> Result<Vec<u8>, BdkError> {
+        handle_mutex(&self.ptr, |psbt| psbt.serialize())
     }
     /// Serialize the PSBT data structure as a String of JSON.
     #[frb(sync)]
-    pub fn json_serialize(&self) -> String {
-        let psbt = self.ptr.lock().unwrap();
-        serde_json::to_string(psbt.deref()).unwrap()
+    pub fn json_serialize(&self) -> Result<String, BdkError> {
+        handle_mutex(&self.ptr, |psbt| {
+            serde_json::to_string(psbt.deref()).map_err(|e| BdkError::Generic(e.to_string()))
+        })?
     }
 }
