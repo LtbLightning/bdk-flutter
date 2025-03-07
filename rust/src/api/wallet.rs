1c6c77349fcb919a0a0a35ce002b2a3d1b17c0f5
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-03-07 17:02:34 -0500

    chore: code cleanup

--- rust/src/api/wallet.rs
+++ rust/src/api/wallet.rs
@@ -1,6 +1,6 @@
 use std::borrow::BorrowMut;
 use std::str::FromStr;
-use std::sync::{ Mutex, MutexGuard };
+use std::sync::{Mutex, MutexGuard};
 
 use bdk_core::bitcoin::Txid;
 use bdk_wallet::PersistedWallet;
@@ -8,53 +8,39 @@ use flutter_rust_bridge::frb;
 
 use crate::api::descriptor::FfiDescriptor;
 
-use super::bitcoin::{ FeeRate, FfiPsbt, FfiScriptBuf, FfiTransaction };
+use super::bitcoin::{FeeRate, FfiPsbt, FfiScriptBuf, FfiTransaction};
 use super::error::{
-    CalculateFeeError,
-    CannotConnectError,
-    CreateWithPersistError,
-    DescriptorError,
-    LoadWithPersistError,
-    SignerError,
-    SqliteError,
-    TxidParseError,
+    CalculateFeeError, CannotConnectError, CreateWithPersistError, DescriptorError,
+    LoadWithPersistError, SignerError, SqliteError, TxidParseError,
 };
 use super::store::FfiConnection;
 use super::types::{
-    AddressInfo,
-    Balance,
-    FfiCanonicalTx,
-    FfiFullScanRequestBuilder,
-    FfiPolicy,
-    FfiSyncRequestBuilder,
-    FfiUpdate,
-    KeychainKind,
-    LocalOutput,
-    Network,
-    SignOptions,
+    AddressInfo, Balance, FfiCanonicalTx, FfiFullScanRequestBuilder, FfiPolicy,
+    FfiSyncRequestBuilder, FfiUpdate, KeychainKind, LocalOutput, Network, SignOptions,
 };
 use crate::frb_generated::RustOpaque;
 
 #[derive(Debug)]
 pub struct FfiWallet {
-    pub opaque: RustOpaque<std::sync::Mutex<bdk_wallet::PersistedWallet<bdk_wallet::rusqlite::Connection>>>,
+    pub opaque:
+        RustOpaque<std::sync::Mutex<bdk_wallet::PersistedWallet<bdk_wallet::rusqlite::Connection>>>,
 }
 impl FfiWallet {
     pub fn new(
         descriptor: FfiDescriptor,
         change_descriptor: FfiDescriptor,
         network: Network,
-        connection: FfiConnection
+        connection: FfiConnection,
     ) -> Result<Self, CreateWithPersistError> {
         let descriptor = descriptor.to_string_with_secret();
         let change_descriptor = change_descriptor.to_string_with_secret();
         let mut binding = connection.get_store();
         let db: &mut bdk_wallet::rusqlite::Connection = binding.borrow_mut();
 
-        let wallet: bdk_wallet::PersistedWallet<bdk_wallet::rusqlite::Connection> = bdk_wallet::Wallet
-            ::create(descriptor, change_descriptor)
-            .network(network.into())
-            .create_wallet(db)?;
+        let wallet: bdk_wallet::PersistedWallet<bdk_wallet::rusqlite::Connection> =
+            bdk_wallet::Wallet::create(descriptor, change_descriptor)
+                .network(network.into())
+                .create_wallet(db)?;
         Ok(FfiWallet {
             opaque: RustOpaque::new(std::sync::Mutex::new(wallet)),
         })
@@ -63,15 +49,14 @@ impl FfiWallet {
     pub fn load(
         descriptor: FfiDescriptor,
         change_descriptor: FfiDescriptor,
-        connection: FfiConnection
+        connection: FfiConnection,
     ) -> Result<Self, LoadWithPersistError> {
         let descriptor = descriptor.to_string_with_secret();
         let change_descriptor = change_descriptor.to_string_with_secret();
         let mut binding = connection.get_store();
         let db: &mut bdk_wallet::rusqlite::Connection = binding.borrow_mut();
 
-        let wallet: PersistedWallet<bdk_wallet::rusqlite::Connection> = bdk_wallet::Wallet
-            ::load()
+        let wallet: PersistedWallet<bdk_wallet::rusqlite::Connection> = bdk_wallet::Wallet::load()
             .descriptor(bdk_wallet::KeychainKind::External, Some(descriptor))
             .descriptor(bdk_wallet::KeychainKind::Internal, Some(change_descriptor))
             .extract_keys()
@@ -84,9 +69,10 @@ impl FfiWallet {
     }
     //TODO; crate a macro to handle unwrapping lock
     pub(crate) fn get_wallet(
-        &self
+        &self,
     ) -> MutexGuard<PersistedWallet<bdk_wallet::rusqlite::Connection>> {
-        self.opaque.lock().expect("wallet")
+        static ERR_MSG: &str = "wallet mutex poisoned";
+        self.opaque.lock().expect(ERR_MSG)
     }
     /// Attempt to reveal the next address of the given `keychain`.
     ///
@@ -96,11 +82,16 @@ impl FfiWallet {
     /// then the last revealed address will be returned.
     #[frb(sync)]
     pub fn reveal_next_address(opaque: FfiWallet, keychain_kind: KeychainKind) -> AddressInfo {
-        opaque.get_wallet().reveal_next_address(keychain_kind.into()).into()
+        opaque
+            .get_wallet()
+            .reveal_next_address(keychain_kind.into())
+            .into()
     }
 
     pub fn apply_update(&self, update: FfiUpdate) -> Result<(), CannotConnectError> {
-        self.get_wallet().apply_update(update).map_err(CannotConnectError::from)
+        self.get_wallet()
+            .apply_update(update)
+            .map_err(CannotConnectError::from)
     }
     /// Get the Bitcoin network the wallet is using.
     #[frb(sync)]
@@ -109,9 +100,10 @@ impl FfiWallet {
     }
     #[frb(sync)]
     pub fn is_mine(&self, script: FfiScriptBuf) -> bool {
-        self.get_wallet().is_mine(
-            <FfiScriptBuf as Into<bdk_core::bitcoin::ScriptBuf>>::into(script)
-        )
+        self.get_wallet()
+            .is_mine(<FfiScriptBuf as Into<bdk_core::bitcoin::ScriptBuf>>::into(
+                script,
+            ))
     }
     /// Return the balance, meaning the sum of this wallet’s unspent outputs’ values. Note that this method only operates
     /// on the internal database, which first needs to be Wallet.sync manually.
@@ -124,10 +116,13 @@ impl FfiWallet {
     pub fn sign(
         opaque: &FfiWallet,
         psbt: FfiPsbt,
-        sign_options: SignOptions
+        sign_options: SignOptions,
     ) -> Result<bool, SignerError> {
         let mut psbt = psbt.opaque.lock().unwrap();
-        opaque.get_wallet().sign(&mut psbt, sign_options.into()).map_err(SignerError::from)
+        opaque
+            .get_wallet()
+            .sign(&mut psbt, sign_options.into())
+            .map_err(SignerError::from)
     }
 
     ///Iterate over the transactions in the wallet.
@@ -141,13 +136,9 @@ impl FfiWallet {
     #[frb(sync)]
     ///Get a single transaction from the wallet as a WalletTx (if the transaction exists).
     pub fn get_tx(&self, txid: String) -> Result<Option<FfiCanonicalTx>, TxidParseError> {
-        let txid = Txid::from_str(txid.as_str()).map_err(|_| TxidParseError::InvalidTxid { txid })?;
-        Ok(
-            self
-                .get_wallet()
-                .get_tx(txid)
-                .map(|tx| tx.into())
-        )
+        let txid =
+            Txid::from_str(txid.as_str()).map_err(|_| TxidParseError::InvalidTxid { txid })?;
+        Ok(self.get_wallet().get_tx(txid).map(|tx| tx.into()))
     }
     pub fn calculate_fee(opaque: &FfiWallet, tx: FfiTransaction) -> Result<u64, CalculateFeeError> {
         opaque
@@ -159,7 +150,7 @@ impl FfiWallet {
 
     pub fn calculate_fee_rate(
         opaque: &FfiWallet,
-        tx: FfiTransaction
+        tx: FfiTransaction,
     ) -> Result<FeeRate, CalculateFeeError> {
         opaque
             .get_wallet()
@@ -173,23 +164,17 @@ impl FfiWallet {
     /// Return the list of unspent outputs of this wallet.
     #[frb(sync)]
     pub fn list_unspent(&self) -> Vec<LocalOutput> {
-        self.get_wallet()
-            .list_unspent()
-            .map(|o| o.into())
-            .collect()
+        self.get_wallet().list_unspent().map(Into::into).collect()
     }
     #[frb(sync)]
     ///List all relevant outputs (includes both spent and unspent, confirmed and unconfirmed).
     pub fn list_output(&self) -> Vec<LocalOutput> {
-        self.get_wallet()
-            .list_output()
-            .map(|o| o.into())
-            .collect()
+        self.get_wallet().list_output().map(Into::into).collect()
     }
     #[frb(sync)]
     pub fn policies(
         opaque: FfiWallet,
-        keychain_kind: KeychainKind
+        keychain_kind: KeychainKind,
     ) -> Result<Option<FfiPolicy>, DescriptorError> {
         opaque
             .get_wallet()
